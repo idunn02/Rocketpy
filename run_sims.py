40bc154634a3a1366e296936e37e969dd2674fe7
Daniel Opara <58013370+bimmui@users.noreply.github.com>
2024-08-14 17:51:02 -0400

    testing rocketpy rocketserializer

---
+++ run_sims.py
@@ -0,0 +1,8 @@
+from rocketpy import Environment, SolidMotor, Rocket, Flight
+import datetime
+import pandas
+import requests
+
+# TODO: Implement a means of taking the longitude and laitude data from json to figure out which weather station
+#          should be used to get sounding air data, this may be farther down the line since you could easily go to the site
+#           yourself and get the data (https://weather.uwyo.edu/upperair/sounding.html)
